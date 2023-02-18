905d7c4768742f2e1769bf274efd24d8044b84c5
Wolf-SO <peu@rekoba.de>
2023-02-19 00:20:21 +0100

    integrate items with controls
    Use `PackableWidget` class to group Panels and their `pack` arguments

--- packsim.py
+++ packsim.py
@@ -1,11 +1,21 @@
 import tkinter as tk
 from tkinter import ttk
 
+class PackableWidget:
+    def __init__(self, widget, pack=False):
+        self.widget = widget
+        self.pack = tk.BooleanVar(value=pack)
+        self.side = tk.StringVar(value="top")
+        self.fill = tk.StringVar(value="none")
+        self.expand = tk.BooleanVar(value=False)
+
 class Application(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        master.geometry("800x600")
-        master.title("tkinter pack layout simulator")
+    def __init__(self):
+        root = tk.Tk()
+        super().__init__(root)
+        root.geometry("800x600")
+        root.title("tkinter pack layout simulator")
+        
         self.item_colors = [
             'IndianRed1', 
             'SteelBlue1', 
@@ -15,86 +25,80 @@ class Application(tk.Frame):
             'LightSteelBlue1', 
             'DarkSeaGreen1', 
             'Khaki1']
-        self.item_count = len(self.item_colors)
         self.default_item_count = 4
+        self.item_count = len(self.item_colors)
 
         self.pack(fill=tk.BOTH, expand=True)
-        
-        self.creates = []
-        self.sides = []
-        self.fills = []
-        self.expands = []
+
         self.items = []
+
+        self.populate_app()
         
-        self.layout_panels()
-        self.layout_control_panel()
         self.layout_items()
 
-    def layout_panels(self):
+    def populate_app(self):
         self.simulation_panel = tk.Frame(self, bg = "white")
         self.control_panel = tk.Frame(self)
         self.control_panel.pack(side=tk.BOTTOM)
         self.simulation_panel.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
         self.contents_frame = tk.Frame(self.simulation_panel, bg="gray31")
         self.contents_frame.pack(side="top", fill="both", expand=True)
-        
-        for x in range(self.item_count):
-            self.items.append(tk.Label(self.contents_frame, text=f"Item {1+x}", bg=self.item_colors[x]))
 
-    def layout_control_panel(self):
+        # row-header for item's pack arguments
         tk.Label(self.control_panel, text="pack").grid(column=0, row=1, sticky="news")
         tk.Label(self.control_panel, text="side").grid(column=0, row=2, sticky="news")
         tk.Label(self.control_panel, text="fill").grid(column=0, row=3, sticky="news")
         tk.Label(self.control_panel, text="expand").grid(column=0, row=4, sticky="news")
         
         for x in range(self.item_count):
-            tk.Label(self.control_panel, text=f"Item {1+x}").grid(column=1+x, row=0)
+            self.add_item(x)
+            
+    def add_item(self, x):
+        # col-header for item controls 
+        tk.Label(self.control_panel, text=f"Item {1+x}").grid(column=1+x, row=0)
 
-            self.creates.append(tk.BooleanVar())
-            self.creates[x].set(x < self.default_item_count)
-            chk1 = ttk.Checkbutton(
-                self.control_panel, 
-                variable=self.creates[x], 
-                command=self.layout_items)
-            chk1.grid(column=1+x, row=1)
+        widget = tk.Label(self.contents_frame, text=f"Item {1+x}", bg=self.item_colors[x])
+        item = PackableWidget(widget, x < self.default_item_count)
+        self.items.append(item)
         
-            self.sides.append(tk.StringVar())
-            self.sides[x].set("top")
-            cb1 = ttk.Combobox(
-                self.control_panel, 
-                textvariable=self.sides[x], 
-                state="readonly", 
-                values=['top', 'bottom', 'left', 'right'], 
-                width=7)
-            cb1.bind("<<ComboboxSelected>>", self.layout_items)
-            cb1.grid(column=1+x, row=2)
+        chk1 = ttk.Checkbutton(
+            self.control_panel, 
+            variable=item.pack, 
+            command=self.layout_items)
+        chk1.grid(column=1+x, row=1)
+    
+        cb1 = ttk.Combobox(
+            self.control_panel, 
+            textvariable=item.side, 
+            state="readonly", 
+            values=['top', 'bottom', 'left', 'right'], 
+            width=7)
+        cb1.bind("<<ComboboxSelected>>", self.layout_items)
+        cb1.grid(column=1+x, row=2)
 
-            self.fills.append(tk.StringVar())
-            self.fills[x].set("none")
-            cb2 = ttk.Combobox(
-                self.control_panel,
-                textvariable=self.fills[x],
-                state="readonly",
-                values=['none', 'x', 'y', 'both'], 
-                width=7)
-            cb2.bind("<<ComboboxSelected>>", self.layout_items)
-            cb2.grid(column=1+x, row=3)
+        cb2 = ttk.Combobox(
+            self.control_panel,
+            textvariable=item.fill,
+            state="readonly",
+            values=['none', 'x', 'y', 'both'], 
+            width=7)
+        cb2.bind("<<ComboboxSelected>>", self.layout_items)
+        cb2.grid(column=1+x, row=3)
 
-            self.expands.append(tk.BooleanVar())
-            self.expands[x].set(False)
-            chk2 = ttk.Checkbutton(
-                self.control_panel,
-                variable=self.expands[x],
-                command=self.layout_items)
-            chk2.grid(column=1+x, row=4)
-
+        chk2 = ttk.Checkbutton(
+            self.control_panel,
+            variable=item.expand,
+            command=self.layout_items)
+        chk2.grid(column=1+x, row=4)
+        
     def layout_items(self,*args):
+        # remove all widgets from frame
         for item in self.items:
-            item.pack_forget()
-        for i, item in enumerate(self.items):
-            if self.creates[i].get():
-                item.pack(side=self.sides[i].get(), fill=self.fills[i].get(), expand=self.expands[i].get())
+            item.widget.pack_forget()
+        # re-pack all widgets to frame
+        for item in self.items:
+            if item.pack.get():
+                item.widget.pack(side=item.side.get(), fill=item.fill.get(), expand=item.expand.get())
 
-root = tk.Tk()
-app = Application(master=root)
+app = Application()
 app.mainloop()
